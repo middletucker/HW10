29d3cfc4ea5001939aaa112b09c1c1abc81150df
middletucker <80505827+middletucker@users.noreply.github.com>
2022-11-01 18:51:24 +0000

    p2 cin

--- .ccls-cache/@home@runner@HW10/P2@main2.cpp
+++ .ccls-cache/@home@runner@HW10/P2@main2.cpp
@@ -8,5 +8,18 @@ using namespace std;
 int main() {
   string month, day, year;
   int HOUR, MIN, hour1, hour2, min1, min2;
+
+  
+    cin >> hour1;
+    cin.ignore();
+    cin >> min1;
+
+    cin >> hour2;
+    cin.ignore();
+    cin >> min2;
+
+    
+
+    
   
 }

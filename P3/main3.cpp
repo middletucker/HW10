cb47d5786e194d4fce36223d65d04e5c8584236e
middletucker <80505827+middletucker@users.noreply.github.com>
2022-11-02 20:28:28 +0000

    P3 setup and gather user input

---
+++ P3/main3.cpp
@@ -0,0 +1,17 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N;
+
+  cin >> N;
+
+  int arrA[N];
+
+  for (int i = 0; i < N; i++)
+    {
+      cin >> arrA[i];
+    }
+}

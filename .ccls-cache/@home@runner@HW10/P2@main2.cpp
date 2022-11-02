6e9df75dcf07b7c7f554491a1e45c107dda7e24a
middletucker <80505827+middletucker@users.noreply.github.com>
2022-11-02 20:05:11 +0000

    P2 add while loop

--- .ccls-cache/@home@runner@HW10/P2@main2.cpp
+++ .ccls-cache/@home@runner@HW10/P2@main2.cpp
@@ -9,22 +9,27 @@ int main() {
   string month, day, year;
   int HOUR, MIN, hour1, hour2, min1, min2;
 
-  cin >> hour1;
-  cin.ignore();
-  cin >> min1;
+  while (cin >> month && cin >> day && cin year) {
+    cin >> hour1;
+    cin.ignore();
+    cin >> min1;
 
-  cin >> hour2;
-  cin.ignore();
-  cin >> min2;
+    cin >> hour2;
+    cin.ignore();
+    cin >> min2;
 
-  HOUR = hour2 - hour1;
+    HOUR = hour2 - hour1;
 
-  if (min2 - min1 < 0) {
-    MIN = (min2 - min1) + 60;
-  } else {
-    MIN = min2 - min1;
+    if (min2 - min1 < 0) {
+      MIN = (min2 - min1) + 60;
+    } else {
+      MIN = min2 - min1;
+    }
+
+    cout << month << " " << day << " " << year << " ";
+    cout << HOUR << " hours " << MIN << " minutes" << endl;
   }
-
-  cout << month << " " << day << " " << year << " ";
-  cout << HOUR << " " << MIN << endl;
 }
+/*
+https://stackoverflow.com/questions/29381224/c-cin-until-no-more-input-on-line-using-a-while-loop
+*/

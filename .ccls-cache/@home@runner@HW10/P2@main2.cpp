02c29f3a2bfcb8d1cc0e1dc60dae4f478a66c76c
middletucker <80505827+middletucker@users.noreply.github.com>
2022-11-02 20:13:24 +0000

    P2 fix curly bracket error, fix hour case

--- .ccls-cache/@home@runner@HW10/P2@main2.cpp
+++ .ccls-cache/@home@runner@HW10/P2@main2.cpp
@@ -9,7 +9,7 @@ int main() {
   string month, day, year;
   int HOUR, MIN, hour1, hour2, min1, min2;
 
-  while (cin >> month && cin >> day && cin year) {
+  while (cin >> month && cin >> day && cin >> year) {
     cin >> hour1;
     cin.ignore();
     cin >> min1;
@@ -22,6 +22,7 @@ int main() {
 
     if (min2 - min1 < 0) {
       MIN = (min2 - min1) + 60;
+      HOUR = HOUR - 1;
     } else {
       MIN = min2 - min1;
     }

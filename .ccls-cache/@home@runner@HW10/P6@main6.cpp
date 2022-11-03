b3804320456a95eeb5d12dedf1853e6306294aa7
middletucker <80505827+middletucker@users.noreply.github.com>
2022-11-03 01:10:36 +0000

    P6 initialize

--- .ccls-cache/@home@runner@HW10/P6@main6.cpp
+++ .ccls-cache/@home@runner@HW10/P6@main6.cpp
@@ -1,13 +1,23 @@
 #include <iomanip>
 #include <iostream>
+#include <string>
 
 using namespace std;
-
-int main() {
   // part A
   struct CarStructure {
     int CarNumber;
     double MilesDriven;
     double GallonsUsed;
-  };
+  } car;
+
+int main() {
+  // part B
+  double MPG = 0, AMPG; // miles per gallon and average mpg
+  CarStructure car[5];
+
+  for (int i = 0; i < 5; i++)
+    {
+      cout << "Enter the numbered car: \n";
+      
+    }
 }

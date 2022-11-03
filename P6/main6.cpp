ea27ba5397da2547a06cee9c1c40ca385c90970a
middletucker <80505827+middletucker@users.noreply.github.com>
2022-11-03 01:17:12 +0000

    P6 user input

--- P6/main6.cpp
+++ P6/main6.cpp
@@ -3,21 +3,33 @@
 #include <string>
 
 using namespace std;
-  // part A
-  struct CarStructure {
-    int CarNumber;
-    double MilesDriven;
-    double GallonsUsed;
-  } car;
+// part A
+struct CarStructure {
+  int CarNumber;
+  double MilesDriven;
+  double GallonsUsed;
+} car;
 
 int main() {
   // part B
   double MPG = 0, AMPG; // miles per gallon and average mpg
   CarStructure car[5];
 
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i < 5; i++) {
+    cout << "Enter the numbered car: \n";
+    cin >> car[i].CarNumber;
+    cout << "Enter number of miles driven: \n";
+    cin >> car[i].MilesDriven;
+    cout << "Enter number of gallons used: \n";
+    cin >> car[i].GallonsUsed;
+  }
+
+  cout << setw(15) << "Car Number"
+       << "MPG \n";
+  cout << "******************************\n";
+
+  for (int j = 0; j < 5; j++)
     {
-      cout << "Enter the numbered car: \n";
-      
+      MPG = += 
     }
 }
